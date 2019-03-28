e30a618f098f05f6e79063700c749399a617f816
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-28 21:54:05 +0000

    Deploy ui.gorilainvest.com.br to github.com/gorilainvest/gorila-ui-components.git:gh-pages

--- docs/js/menu-wc.js
+++ docs/js/menu-wc.js
@@ -16,7 +16,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">gorila-app-widgets-system documentation</a>
+                    <a href="index.html" data-type="index-link">@gorilainvest/ui-toolkit documentation</a>
                 </li>
 
                 <li class="divider"></li>
