453cf17d76239beeb93be588531a4fa0b790c504
Gois <guijocargo@gmail.com>
2019-04-23 17:47:11 -0300

    build: fix directives build for publish

--- libs/directives/src/index.ts
+++ libs/directives/src/index.ts
@@ -1,6 +1,1 @@
-export * from './lib/cancelable-router-link-with-href/cancelable-router-link-with-href.directive';
-export * from './lib/directives.module';
-export * from './lib/tippy/tippy.directive';
-export * from './lib/tippy/tippy.helper';
-export * from './lib/tippy/tippy.model';
-export * from './lib/truncate/truncate.directive';
+export * from './public_api';
