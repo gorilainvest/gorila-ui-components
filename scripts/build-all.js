576415669a27be7e98e8923458e6709262c97cf0
Gois <guijocargo@gmail.com>
2019-08-26 11:09:21 -0300

    build: improves build script to rely on nx.json information

--- scripts/build-all.js
+++ scripts/build-all.js
@@ -2,33 +2,18 @@ const ngPackage = require('ng-packagr');
 const rimraf = require('rimraf');
 const Rx = require('rxjs');
 const RxOp = require('rxjs/operators');
-const angularjson = require('../angular.json');
+const nxjson = require('../nx.json');
 
 const clog = msg => console.log('\x1b[36m%s\x1b[0m', msg);
 
-const COMPILED_LIBS = Object.keys(angularjson.projects)
-  .filter(lib => lib.startsWith('ui-') || lib === 'directives')
+const COMPILED_LIBS = Object.keys(nxjson.projects)
+  .filter(lib => nxjson.projects[lib].tags.find(tag => tag === 'exportable'))
   .reduce((acc, lib) => ({ ...acc, [lib]: false }), {});
 
-const DEP_GRAPH = {
-  'ui-card-container': ['ui-section-title'],
-  'ui-countdown-button': ['ui-button'],
-  'ui-loading': ['ui-spinner']
-};
+const DEP_GRAPH = Object.keys(COMPILED_LIBS)
+  .reduce((acc, k) => ({ ...acc, [k]: nxjson.projects[k].implicitDependencies }));
 
-const libs = [
-  'directives',
-  'ui-arrow-variation',
-  'ui-button',
-  'ui-card-container',
-  'ui-countdown-button',
-  'ui-enhanced-title',
-  'ui-loading',
-  'ui-popup-confirm',
-  'ui-section-title',
-  'ui-slide-toggle',
-  'ui-spinner'
-];
+const libs = Object.keys(COMPILED_LIBS);
 
 rimraf('dist', { disableGlob: true }, async err => {
   if (err) {
