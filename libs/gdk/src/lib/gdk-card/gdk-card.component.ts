bc4d81077a5974932dcacbfd8dabac028a5a2a84
Gois <guijocargo@gmail.com>
2019-08-28 19:03:17 -0300

    docs: add minimal documentation to gdk-card

--- libs/gdk/src/lib/gdk-card/gdk-card.component.ts
+++ libs/gdk/src/lib/gdk-card/gdk-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
 
+/**
+ * A card to be used for shell libs with input and output aside, a preview and code areas.
+ */
 @Component({
   selector: "gdk-card",
   templateUrl: "./gdk-card.component.html",
@@ -7,14 +10,32 @@ import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
   encapsulation: ViewEncapsulation.None
 })
 export class GdkCardComponent implements OnInit {
+  /**
+   * The title for the card.
+   */
   @Input() title: string;
+
+  /**
+   * The subtitle for the card.
+   */
   @Input() subtitle: string;
+
+  /**
+   * Flag to control right aside for summary info.
+   */
   @Input() summary: boolean;
+
+  /**
+   * Flag to not show inputs in aside.
+   */
   @Input() withoutInput: boolean;
+
+  /**
+   * Flag to not show outputs in aside.
+   */
   @Input() withoutOutput: boolean;
 
   constructor() {}
 
   ngOnInit() {}
 }
-
