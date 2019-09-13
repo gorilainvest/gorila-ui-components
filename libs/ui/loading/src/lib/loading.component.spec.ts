2969aaf27b867fa79667cc50d5b94c72b9493df0
Denis Okawa <denis.okawa@bxchain.io>
2019-09-13 19:37:53 -0300

    test(loading): create tests for loading component
    GORPP-3332 #done

--- libs/ui/loading/src/lib/loading.component.spec.ts
+++ libs/ui/loading/src/lib/loading.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from "@angular/platform-browser";
 
 import { LoadingComponent } from './loading.component';
 
@@ -21,4 +22,25 @@ describe('LoadingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display message passed', () => {
+    component.message = 'test message';
+    fixture.detectChanges();
+    const message = fixture.debugElement.query(By.css('#message')).nativeElement.textContent.trim();
+    expect(message).toBe('test message');
+  });
+
+  it('should display component when stopLoading === false', () => {
+    component.stopLoading = false;
+    fixture.detectChanges();
+    const spinner = fixture.debugElement.query(By.css('.loading-spinner'));
+    expect(spinner).toBeTruthy();
+  })
+
+  it('should hide component when stopLoading === true', () => {
+    component.stopLoading = true;
+    fixture.detectChanges();
+    const spinner = fixture.debugElement.query(By.css('.loading-spinner'));
+    expect(spinner).toBeFalsy();
+  })
 });
