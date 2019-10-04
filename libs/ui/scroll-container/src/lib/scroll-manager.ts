b5190a4db6e3bed64bd02674b4dd97aacdaa0536
Guilherme Gois <guilherme.gois@gorilainvest.com.br>
2019-10-04 16:03:34 -0300

    docs(scroll-container): add documentation to scroll-manager.ts

--- libs/ui/scroll-container/src/lib/scroll-manager.ts
+++ libs/ui/scroll-container/src/lib/scroll-manager.ts
@@ -2,12 +2,28 @@ import { CdkScrollable, ScrollDispatcher } from '@angular/cdk/scrolling';
 import { untilDestroyed } from 'ngx-take-until-destroy';
 import { Subscription } from 'rxjs';
 
+/**
+ * Define a interface to guarantee a subscription instance to be managed
+ * in seupScrollSubscription.
+ */
 export interface Scrollable {
+  /**
+   * The subscription for *Scrolldispatcher.scrolled* method call.
+   */
   scrolledSubs: Subscription;
 }
 
 export type Edge = 'top' | 'left' | 'right' | 'bottom' | 'start' | 'end';
 
+/**
+ * Setups a new scroll subscription, also unsubscribe from previous subscriptions
+ * if needed.
+ *
+ * @param activate indicates if a new subscription must be created
+ * @param componentRef a **scrollable** component that implements `Scrollable` interface
+ * @param scrollDispatcher the responsible to listen for scroll events
+ * @param windowScrollFn a callback for scroll subscription
+ */
 export const setupScrollSubscription = <C extends Scrollable>(
   activate: boolean,
   componentRef: C,
@@ -28,8 +44,18 @@ export const setupScrollSubscription = <C extends Scrollable>(
     .subscribe(windowScrollFn.bind(componentRef));
 };
 
+/**
+ * A interface to provide information about the last scroll event received.
+ */
 export interface ScrollInfo {
+  /**
+   * How many pixels was scrolled.
+   */
   offset: number;
+
+  /**
+   * Indicates that the scroll was detected.
+   */
   scrolled: boolean;
 }
 
