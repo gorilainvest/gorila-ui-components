7c60416742844e957a28554a3e20337d884e50e0
Gustavo Oliveira <gustavo.oliveira@gorilainvest.com.br>
2019-11-12 11:16:08 -0300

    docs(page-header): add descriptions to page-header inputs

--- libs/ui-toolkit/page-header/src/lib/page-header.component.ts
+++ libs/ui-toolkit/page-header/src/lib/page-header.component.ts
@@ -22,10 +22,46 @@ import { Subscription } from 'rxjs';
   encapsulation: ViewEncapsulation.None
 })
 export class PageHeaderComponent implements OnDestroy, OnInit, Scrollable {
+  /**
+   * Indicates that the scroll effect must be activated.
+   *
+   * @default true
+   */
+
   @Input() public activateScrollEffect = true;
+
+  /**
+   * Indicates the distance from the edge (as measured in pixels)
+   * from which the scroll effect will be activated.
+   *
+   * @default 100
+   */
+
   @Input() public departureOffset = 100;
+
+  /**
+   * Indicates the edge from which the offset distances will be measured.
+   *
+   * @default 'top'
+   */
+
   @Input() public edge: 'top' | 'left' | 'right' | 'bottom' | 'start' | 'end' = 'top';
+
+  /**
+   * Indicates the distance from the edge (as measured in pixels)
+   * from which the scroll effect will be deactivated.
+   *
+   * @default 10
+   */
+
   @Input() public returnOffset = 10;
+
+  /**
+   * Indicates that the bottom area of the page header will be displayed.
+   *
+   * @default true
+   */
+
   @Input() public enableBottom = true;
 
   @HostBinding('class.scrolled') public scrolled: boolean;
