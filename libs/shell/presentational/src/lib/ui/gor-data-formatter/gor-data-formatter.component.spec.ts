7a7fd4c3a3e4cb22e91db2949860d96a7830ad3f
Thompson Filgueiras <tigredonorte3@gmail.com>
2019-11-13 09:52:13 -0300

    fix: fix gor-data-formatter unit tests

--- libs/shell/presentational/src/lib/ui/gor-data-formatter/gor-data-formatter.component.spec.ts
+++ libs/shell/presentational/src/lib/ui/gor-data-formatter/gor-data-formatter.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { GorDataFormatterModule } from '@gorilainvest/ui-toolkit/gor-data-formatter';
 
 import { GorDataFormatterComponent } from './gor-data-formatter.component';
 
@@ -8,7 +10,7 @@ describe('GorDataFormatterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [GorDataFormatterComponent]
+      imports: [GorDataFormatterModule, NoopAnimationsModule]
     }).compileComponents();
   }));
 
