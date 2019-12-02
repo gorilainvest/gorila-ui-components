ca6206e9bbf460c7bdf0fe5ac09dcaf89fbe081b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-02 19:12:46 +0000

    Deploy ui.gorilainvest.com.br to github.com/gorilainvest/gorila-ui-components.git:gh-pages

--- docs/js/menu-wc.js
+++ docs/js/menu-wc.js
@@ -374,6 +374,24 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                         </ul>
                                     </li>
                             </li>
+                            <li class="link">
+                                <a href="modules/UiIconModule.html" data-type="entity-link">UiIconModule</a>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                            'data-target="#components-links-module-UiIconModule-a3a76f206f443cd3715313d542df1929"' : 'data-target="#xs-components-links-module-UiIconModule-a3a76f206f443cd3715313d542df1929"' }>
+                                            <span class="icon ion-md-cog"></span>
+                                            <span>Components</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-UiIconModule-a3a76f206f443cd3715313d542df1929"' :
+                                            'id="xs-components-links-module-UiIconModule-a3a76f206f443cd3715313d542df1929"' }>
+                                            <li class="link">
+                                                <a href="components/UiIconComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">UiIconComponent</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                            </li>
                 </ul>
                 </li>
                     <li class="chapter">
