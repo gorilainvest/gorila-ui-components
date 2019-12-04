0325c288f90be038ae9d2a1e1936348c5ff0beab
Gustavo Oliveira <gustavo.oliveira@gorilainvest.com.br>
2019-12-04 17:56:32 -0300

    test(icon): add missing import in icon test

--- libs/shell/layout/src/lib/ui/icon/icon.component.spec.ts
+++ libs/shell/layout/src/lib/ui/icon/icon.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LayoutShellModule } from '../../layout-shell.module';
 import { IconComponent } from './icon.component';
@@ -9,7 +10,7 @@ describe('IconComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [LayoutShellModule]
+      imports: [LayoutShellModule, NoopAnimationsModule]
     }).compileComponents();
   }));
 
