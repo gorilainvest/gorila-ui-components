1a2252f2a83e61b871c592e02a244a41b2f20fbc
Gustavo Oliveira <gustavo.oliveira@gorilainvest.com.br>
2019-12-06 10:23:59 -0300

    feat(sort-header): create sort-header component

---
+++ libs/ui-toolkit/sort-header/src/lib/sort-header.component.spec.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSortModule } from '@angular/material/sort';
+import { IconModule } from '@gorilainvest/ui-toolkit/icon';
+
+import { SortHeaderComponent } from './sort-header.component';
+
+describe('SortHeaderComponent', () => {
+  let component: SortHeaderComponent;
+  let fixture: ComponentFixture<SortHeaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        MatSortModule,
+        IconModule
+      ],
+      declarations: [SortHeaderComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SortHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
