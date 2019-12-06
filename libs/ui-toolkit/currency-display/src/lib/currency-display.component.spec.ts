04fb2a39c030e921c85ed4a41e778845a794aadd
Thompson Filgueiras <tigredonorte3@gmail.com>
2019-12-06 11:44:12 -0300

    test(currency-display): added currency-display unit tests

--- libs/ui-toolkit/currency-display/src/lib/currency-display.component.spec.ts
+++ libs/ui-toolkit/currency-display/src/lib/currency-display.component.spec.ts
@@ -1,4 +1,6 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { CurrencyDisplayComponent } from './currency-display.component';
 import { CurrencyDisplayModule } from './currency-display.module';
@@ -11,6 +13,9 @@ describe('CurrencyDisplayComponent', () => {
     TestBed.configureTestingModule({
       imports: [CurrencyDisplayModule]
     })
+    .overrideComponent(CurrencyDisplayComponent, {
+      set: { changeDetection: ChangeDetectionStrategy.Default }
+    })
     .compileComponents();
   }));
 
@@ -22,5 +27,78 @@ describe('CurrencyDisplayComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('CurrencyCode -', () => {
+    it('expect invalid currency code to be displayed itself', () => {
+      component.currencyCode = 'foo';
+      component.ngOnChanges();
+      fixture.detectChanges();
+      const symbol = fixture.debugElement.query(By.css('.currency-text-symbol')).nativeElement.textContent.trim();
+      expect(symbol).toEqual('foo');
+    });
+
+    it('expect BRL input to display R$', () => {
+      component.currencyCode = 'BRL';
+      component.ngOnChanges();
+      fixture.detectChanges();
+      const symbol = fixture.debugElement.query(By.css('.currency-text-symbol')).nativeElement.textContent.trim();
+      expect(symbol).toEqual('R$');
+    });
+  });
+
+  describe('SymbolDisplay -', () => {
+    it('expect symbolDisplay=symbol - BRL to be R$', () => {
+      component.symbolDisplay = 'symbol';
+      component.currencyCode = 'BRL';
+      component.ngOnChanges();
+      fixture.detectChanges();
+      const symbol = fixture.debugElement.query(By.css('.currency-text-symbol')).nativeElement.textContent.trim();
+      expect(symbol).toEqual('R$');
+    });
+
+    it('expect symbolDisplay=code - BRL to be BRL', () => {
+      component.symbolDisplay = 'code';
+      component.currencyCode = 'BRL';
+      component.ngOnChanges();
+      fixture.detectChanges();
+      const symbol = fixture.debugElement.query(By.css('.currency-text-symbol')).nativeElement.textContent.trim();
+      expect(symbol).toEqual('BRL');
+    });
+  });
+
+  describe('DigitInfo -', () => {
+    it('expect 1.1-2 to display value 0.0', () => {
+      component.digitInfo = '1.1-2';
+      component.ngOnChanges();
+      fixture.detectChanges();
+      const value = fixture.debugElement.query(By.css('.currency-text-value')).nativeElement.textContent.trim();
+      expect(value).toEqual('0.0');
+    });
+    it('expect 3.0-0 to display value 000', () => {
+      component.digitInfo = '3.0-0';
+      component.ngOnChanges();
+      fixture.detectChanges();
+      const value = fixture.debugElement.query(By.css('.currency-text-value')).nativeElement.textContent.trim();
+      expect(value).toEqual('000');
+    });
+  });
+  describe('HideNegative -', () => {
+    it('expect negative number to be displayed', () => {
+      component.hideNegative = false;
+      component.value = -5;
+      component.ngOnChanges();
+      fixture.detectChanges();
+      const value = fixture.debugElement.query(By.css('.currency-text-symbol')).nativeElement.textContent.trim();
+      expect(value.includes('-')).toBeTruthy();
+    });
+    it('expect negative number to be hided', () => {
+      component.hideNegative = true;
+      component.value = -5;
+      component.ngOnChanges();
+      fixture.detectChanges();
+      const value = fixture.debugElement.query(By.css('.currency-text-symbol')).nativeElement.textContent.trim();
+      expect(value.includes('-')).toBeFalsy();
+    });
   });
 });
