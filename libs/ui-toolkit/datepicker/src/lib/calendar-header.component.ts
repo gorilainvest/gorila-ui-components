f324dd47f22c0a94ece3988fd8fd6ebf66884c68
Guilherme Gois <guilherme.gois@gorilainvest.com.br>
2020-01-02 19:28:03 -0300

    docs(datepicker): add documentation

--- libs/ui-toolkit/datepicker/src/lib/calendar-header.component.ts
+++ libs/ui-toolkit/datepicker/src/lib/calendar-header.component.ts
@@ -4,6 +4,11 @@ import { MatCalendarHeader } from '@angular/material/datepicker';
 const LEFT_ARROW_D = 'M7.2 9.688a.348.348 0 0 1-.247-.102l-4.35-4.34a.349.349 0 0 1 0-.493L6.952.414a.349.349 0 1 1 .492.494L3.342 5l4.103 4.092a.349.349 0 0 1-.246.595';
 const RIGHT_ARROW_D = 'M2.849 9.688a.349.349 0 0 1-.246-.596L6.704 5 2.602.908a.349.349 0 1 1 .493-.494l4.35 4.34a.349.349 0 0 1 0 .493l-4.35 4.339a.348.348 0 0 1-.246.101';
 
+/**
+ * Default header for calendar of GorDatepicker.
+ *
+ * @extends MatCalendarHeader
+ */
 @Component({
   selector: 'gor-calendar-header',
   template: `
@@ -28,6 +33,17 @@ const RIGHT_ARROW_D = 'M2.849 9.688a.349.349 0 0 1-.246-.596L6.704 5 2.602.908a.
   `]
 })
 export class CalendarHeaderComponent<D> extends MatCalendarHeader<D> {
+  /**
+   * A valid text color for period button.
+   *
+   * @default "#2d2d2d"
+   */
   @Input() public color = '#2d2d2d';
+
+  /**
+   * A valid fill color for next and previous buttons.
+   *
+   * @default "#888e8e"
+   */
   @Input() public colorCtrls = '#888E8E';
 }
--- libs/ui-toolkit/datepicker/src/lib/datepicker.component.ts
+++ libs/ui-toolkit/datepicker/src/lib/datepicker.component.ts
@@ -7,6 +7,8 @@ import { CalendarHeaderComponent } from './calendar-header.component';
 export const SELECTOR = 'gor-datepicker';
 
 /**
+ * Component responsible to display calendar and date list (in future).
+ *
  * @link https://github.com/angular/components/blob/master/src/material/datepicker/calendar.ts
  */
 @Component({
@@ -20,23 +22,67 @@ export class DatepickerComponent implements AfterViewInit {
 
   @HostBinding('style.background-color') @Input() bgColor = 'white';
 
+  /**
+   * The text to main button of datepicker
+   *
+   * @default "Apply"
+   */
   @Input() public applyText = 'Apply';
+
+  /**
+   * An input indicating the type of the custom header component for the calendar, if set.
+   *
+   * @default CalendarHeaderComponent
+   */
   @Input() public headerComponentRef: ComponentType<any> = CalendarHeaderComponent;
+
+  /**
+   * The maximun range date to be selected.
+   *
+   * @default today
+   */
   @Input() public maxDate: Moment = utc();
+
+  /**
+   * The minimun range date to be selected.
+   *
+   * @default 5 years before today
+   */
   @Input() public minDate: Moment = utc().subtract(5, 'years');
+
+  /**
+   * The active day when datepicker is opened.
+   *
+   * @default today
+   */
   @Input() public startAt: Moment = utc();
+
+  /**
+   * The active view when datepicker is opened.
+   *
+   * @default "month"
+   * @see MatCalendarView
+   */
   @Input() public startView: MatCalendarView = 'month';
 
   @HostBinding('class') public hostClass = SELECTOR;
 
+  /**
+   * The selected date stored for internal use.
+   *
+   * @default today
+   */
   public selectedDate: Moment | null = utc();
 
-  constructor() {}
-
-  ngAfterViewInit() {
+  public ngAfterViewInit() {
     this._calendar.focusActiveCell();
   }
 
+  /**
+   * Function to be executed when some date is selected.
+   *
+   * @param newDate
+   */
   public dateChanged(newDate: Moment | null) {
     this.selectedDate = newDate;
   }
