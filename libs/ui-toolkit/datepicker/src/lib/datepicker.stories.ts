0c794b239e0aba1aeb1b5ec2a468dc2e86733eea
Guilherme Gois <guilherme.gois@gorilainvest.com.br>
2020-01-02 16:41:51 -0300

    docs(datepicker): add stories for start month property

--- libs/ui-toolkit/datepicker/src/lib/datepicker.stories.ts
+++ libs/ui-toolkit/datepicker/src/lib/datepicker.stories.ts
@@ -1,11 +1,21 @@
-import { MODULE_METADATA } from './datepicker.module';
 import { storiesOf, moduleMetadata } from '@storybook/angular';
-import { DatepickerComponent } from './datepicker.component';
+
+import { MODULE_METADATA } from './datepicker.module';
 
 storiesOf('Datepicker', module)
   .addDecorator(moduleMetadata(MODULE_METADATA))
-  .add('dafault view', () => ({
+  .add('starting in month view', () => ({
     template: `
-    <gor-datepicker></gor-datepicker>
+    <gor-datepicker startView='month'></gor-datepicker>
+    `
+  }))
+  .add('starting in multi-year view', () => ({
+    template: `
+    <gor-datepicker startView='multi-year'></gor-datepicker>
+    `
+  }))
+  .add('starting in year view', () => ({
+    template: `
+    <gor-datepicker startView='year'></gor-datepicker>
     `
   }));
