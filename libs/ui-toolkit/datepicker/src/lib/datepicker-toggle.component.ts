a6ff99e7a71f6b191b9063ea2101e6d1085e3170
Guilherme Gois <guilherme.gois@gorilainvest.com.br>
2020-01-03 14:51:25 -0300

    docs(datepicker): add documentation to toggle component

--- libs/ui-toolkit/datepicker/src/lib/datepicker-toggle.component.ts
+++ libs/ui-toolkit/datepicker/src/lib/datepicker-toggle.component.ts
@@ -3,6 +3,11 @@ import { MatDatepickerToggle } from '@angular/material/datepicker';
 
 export const SELECTOR = 'gor-datepicker-toggle';
 
+/**
+ * Extends MatDatepickerToggle to insert our custom calendar icon.
+ * 
+ * @link https://github.com/angular/components/blob/master/src/material/datepicker/datepicker-toggle.ts
+ */
 @Component({
   selector: SELECTOR,
   templateUrl: './datepicker-toggle.component.html',
@@ -11,11 +16,34 @@ export const SELECTOR = 'gor-datepicker-toggle';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DatepickerToggleComponent<D> extends MatDatepickerToggle<D> {
-  @HostBinding('attr.tabindex') public readonly = '-1';
+  /**
+   * Set readonly tabIndex for toggle component.
+   */
+  @HostBinding('attr.tabindex') public readonly tabIndex= '-1';
+  
+  /**
+   * Set a css class equal to component selector.
+   */
   @HostBinding('class') public readonly hostClass = SELECTOR;
+  
+  /**
+   * Indicates if associated datepicker is active.
+   */
   @HostBinding('class.mat-datepicker-toggle-active') public readonly active = this.datepicker && this.datepicker.opened;
+
+  /**
+   * Indicates if current datepicker color palette is accent.
+   */
   @HostBinding('class.mat-accent') public readonly accentColor = this.datepicker && this.datepicker.color === 'accent';
+  
+  /**
+   * Indicates if current datepicker color palette is warn.
+   */
   @HostBinding('class.mat-warn') public readonly warnColor = this.datepicker && this.datepicker.color === 'warn';
+
+  /**
+   * Listen to focus event on component to force focus on button.
+   */
   @HostListener('focus') public setFocus() {
     this._button.focus();
   }
