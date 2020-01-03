7940f89d80b08a6fdfe35de7b6e782e80f0cbb6b
Guilherme Gois <guilherme.gois@gorilainvest.com.br>
2020-01-03 17:54:23 -0300

    docs(datepicker): add stories for max and min dates

--- libs/ui-toolkit/datepicker/src/lib/datepicker.stories.ts
+++ libs/ui-toolkit/datepicker/src/lib/datepicker.stories.ts
@@ -1,6 +1,9 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
+import { localeData, utc } from 'moment';
 
 import { MODULE_METADATA } from './datepicker.module';
+
+const today = utc().format('YYYY-MM-DD');
 
 storiesOf('Datepicker', module)
   .addDecorator(moduleMetadata(MODULE_METADATA))
@@ -17,5 +20,15 @@ storiesOf('Datepicker', module)
   .add('starting in year view', () => ({
     template: `
     <gor-datepicker-content startView='year'></gor-datepicker-content>
+    `
+  }))
+  .add('max date set to today', () => ({
+    template: `
+    <gor-datepicker-content maxDate='${today}'></gor-datepicker-content>
+    `
+  }))
+  .add('min date set to today', () => ({
+    template: `
+    <gor-datepicker-content minDate='${today}'></gor-datepicker-content>
     `
   }));
