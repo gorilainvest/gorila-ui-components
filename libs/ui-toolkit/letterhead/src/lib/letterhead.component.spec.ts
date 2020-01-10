cb7cbe2030d7d00c74b77b5dce5ad30d0ec15c07
Gustavo C. Oliveira <gustavo.oliveira@gorilainvest.com.br>
2020-01-10 13:57:31 -0300

    test(letterhead): fix component import

--- libs/ui-toolkit/letterhead/src/lib/letterhead.component.spec.ts
+++ libs/ui-toolkit/letterhead/src/lib/letterhead.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { LetterheadComponent } from './mur-letterhead.component';
+import { LetterheadComponent } from './letterhead.component';
 import { LetterheadFooterComponent } from './footer/footer.component';
 import { LetterheadHeaderComponent } from './header/header.component';
 
