d02349c6f70a899d7494dcf288854396d70ae34f
Gustavo Oliveira <gustavo.oliveira@gorilainvest.com.br>
2020-02-14 16:26:53 -0300

    feat(checkbox): create more story cases

--- libs/ui-toolkit/checkbox/src/lib/checkbox.stories.ts
+++ libs/ui-toolkit/checkbox/src/lib/checkbox.stories.ts
@@ -8,18 +8,80 @@ storiesOf('Checkbox', module)
     })
   )
   .add(
-    'playground',
+    'gradient background',
     () => ({
-      template: `
-      <div style="display: flex; flex-direction: column;">
-        <gor-checkbox [size]="'sm'" [labelTransform]="'up'" [backgroundColor]="'red'" [checked]="true"></gor-checkbox>
-        <gor-checkbox [size]="'sm'" [labelTransform]="'up'" [backgroundColor]="'darkblue'" [checked]="true"></gor-checkbox>
-        <gor-checkbox [size]="'sm'" [labelTransform]="'up'" [backgroundColor]="'blue'" [checked]="true"></gor-checkbox>
-        <gor-checkbox [size]="'sm'" [labelTransform]="'up'" [backgroundColor]="'lightblue'" [checked]="true"></gor-checkbox>
-        <gor-checkbox [size]="'md'" [labelTransform]="'lo'" [textColor]="'light'" [backgroundColor]="'pink'" [checked]="true"></gor-checkbox>
-        <gor-checkbox [size]="'lg'" [labelTransform]="'cp'" [backgroundColor]="'yellow'" [checked]="true"></gor-checkbox>
-        <gor-checkbox [size]="'lg'" [labelTransform]="'no'" [label]="'LaBeL'" [textColor]="'light'" [backgroundColor]="'gradient'"  [checked]="true"></gor-checkbox>
-      </div>
-      `
+      template: `<gor-checkbox [backgroundColor]="'gradient'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'red background',
+    () => ({
+      template: `<gor-checkbox [backgroundColor]="'red'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'darkblue background',
+    () => ({
+      template: `<gor-checkbox [backgroundColor]="'darkblue'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'blue background',
+    () => ({
+      template: `<gor-checkbox [backgroundColor]="'blue'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'lightblue background',
+    () => ({
+      template: `<gor-checkbox [backgroundColor]="'lightblue'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'pink background',
+    () => ({
+      template: `<gor-checkbox [backgroundColor]="'pink'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'yellow background',
+    () => ({
+      template: `<gor-checkbox [backgroundColor]="'yellow'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'lowercase',
+    () => ({
+      template: `<gor-checkbox [labelTransform]="'lo'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'uppercase',
+    () => ({
+      template: `<gor-checkbox [labelTransform]="'up'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'capitalize',
+    () => ({
+      template: `<gor-checkbox [labelTransform]="'cp'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'none',
+    () => ({
+      template: `<gor-checkbox [label]="'LaBeL'" [labelTransform]="'no'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'dark text',
+    () => ({
+      template: `<gor-checkbox [textColor]="'dark'" [size]="'lg'"></gor-checkbox>`
+    })
+  )
+  .add(
+    'light text',
+    () => ({
+      template: `<gor-checkbox [textColor]="'light'" [size]="'lg'"></gor-checkbox>`
     })
   )
