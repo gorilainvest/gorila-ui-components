aeb21b7739a65c85afa1a9777257c3e23a85ce6d
Gustavo Oliveira <gustavo.oliveira@gorilainvest.com.br>
2020-02-14 16:57:44 -0300

    test(checkbox): fix testing module setup

--- libs/ui-toolkit/checkbox/src/lib/checkbox.component.spec.ts
+++ libs/ui-toolkit/checkbox/src/lib/checkbox.component.spec.ts
@@ -1,4 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { CheckboxComponent } from './checkbox.component';
 
@@ -8,6 +11,11 @@ describe('CheckboxComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        FormsModule,
+        MatCheckboxModule,
+      ],
       declarations: [CheckboxComponent]
     }).compileComponents();
   }));
