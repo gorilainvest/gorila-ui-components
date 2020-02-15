d55d2d483d1db2d92d75be0d1beace45bb07b968
Gustavo C. Oliveira <gustavo.oliveira@gorilainvest.com.br>
2020-02-14 22:03:41 -0300

    refactor(checkbox): start all stories in checked state

--- libs/ui-toolkit/checkbox/src/lib/checkbox.stories.ts
+++ libs/ui-toolkit/checkbox/src/lib/checkbox.stories.ts
@@ -10,96 +10,96 @@ storiesOf('Checkbox', module)
   .add(
     'gradient background',
     () => ({
-      template: `<gor-checkbox [backgroundColor]="'gradient'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [backgroundColor]="'gradient'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'red background',
     () => ({
-      template: `<gor-checkbox [backgroundColor]="'red'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [backgroundColor]="'red'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'darkblue background',
     () => ({
-      template: `<gor-checkbox [backgroundColor]="'darkblue'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [backgroundColor]="'darkblue'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'blue background',
     () => ({
-      template: `<gor-checkbox [backgroundColor]="'blue'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [backgroundColor]="'blue'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'lightblue background',
     () => ({
-      template: `<gor-checkbox [backgroundColor]="'lightblue'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [backgroundColor]="'lightblue'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'pink background',
     () => ({
-      template: `<gor-checkbox [backgroundColor]="'pink'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [backgroundColor]="'pink'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'yellow background',
     () => ({
-      template: `<gor-checkbox [backgroundColor]="'yellow'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [backgroundColor]="'yellow'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'lowercase',
     () => ({
-      template: `<gor-checkbox [labelTransform]="'lo'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [labelTransform]="'lo'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'uppercase',
     () => ({
-      template: `<gor-checkbox [labelTransform]="'up'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [labelTransform]="'up'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'capitalize',
     () => ({
-      template: `<gor-checkbox [labelTransform]="'cp'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [labelTransform]="'cp'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'none',
     () => ({
-      template: `<gor-checkbox [label]="'LaBeL'" [labelTransform]="'no'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [label]="'LaBeL'" [labelTransform]="'no'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'dark text',
     () => ({
-      template: `<gor-checkbox [textColor]="'dark'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [textColor]="'dark'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'light text',
     () => ({
-      template: `<gor-checkbox [textColor]="'light'" [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [textColor]="'light'" [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'small',
     () => ({
-      template: `<gor-checkbox [size]="'sm'"></gor-checkbox>`
+      template: `<gor-checkbox [size]="'sm'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'medium',
     () => ({
-      template: `<gor-checkbox [size]="'md'"></gor-checkbox>`
+      template: `<gor-checkbox [size]="'md'" [checked]="true"></gor-checkbox>`
     })
   )
   .add(
     'large',
     () => ({
-      template: `<gor-checkbox [size]="'lg'"></gor-checkbox>`
+      template: `<gor-checkbox [size]="'lg'" [checked]="true"></gor-checkbox>`
     })
   )
