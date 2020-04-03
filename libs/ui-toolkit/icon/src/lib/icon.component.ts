6a2f69d91fec39a8521b8b79b66fc9de5d043a0d
Gustavo C. Oliveira <gustavo.oliveira@gorilainvest.com.br>
2020-04-03 11:31:56 -0300

    refactor(icon): update icon component to use base path from token and create fal

--- libs/ui-toolkit/icon/src/lib/icon.component.ts
+++ libs/ui-toolkit/icon/src/lib/icon.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, Input } from '@angular/core';
+
+import { ICON_BASE_SRC } from './token';
 
 /**
  * Displays an icon.
@@ -14,6 +16,7 @@ export class IconComponent {
    * The color modifier for the icon path.
    */
   @Input() public set color(data: string) {
+    this.imageError = false;
     this._color = data;
     this.updatePath();
   }
@@ -22,24 +25,34 @@ export class IconComponent {
    * The icon modifier for the icon path.
    */
   @Input() public set icon(data: string) {
+    this.imageError = false;
     this._icon = data;
     this.updatePath();
   }
 
   /**
-   * Base prefix for the icon path.
+   * Secondary option for icon base URL/path.
    */
-  @Input() public set pathPrefix(data: string) {
-    this._pathPrefix = data;
+  @Input() public set secondaryBase(data: string) {
+    this.imageError = false;
+    this._secondaryBase = data;
+    this.updatePath();
+  }
+
+  /**
+   * The folder modifier for the icon path.
+   */
+  @Input() public set iconFolder(data: string) {
+    this.imageError = false;
+    this._iconFolder = data + '/';
     this.updatePath();
   }
 
   /**
    * Icon's image file format.
-   *
-   * @default svg
    */
   @Input() public set imgFormat(data: string) {
+    this.imageError = false;
     this._imgFormat = data;
     this.updatePath();
   }
@@ -65,25 +78,38 @@ export class IconComponent {
 
   /**
    * The path for the icon's image file.
+   *
+   * @default ''
    */
   public path = '';
 
   /**
    * Stores data of color modifier for the icon path.
+   *
+   * @default null
    */
   private _color = null;
 
   /**
    * Stores data of icon modifier for the icon path.
+   *
+   * @default ''
    */
   private _icon = '';
 
   /**
-   * Stores data of path prefix for the icon path.
+   * Stores data of secondary path prefix for the icon path.
    *
    * @default '/assets/img/'
    */
-  private _pathPrefix = '/assets/img/';
+  private _secondaryBase = '/assets/img/';
+
+  /**
+   * Stores data of folder modifier for the icon path.
+   *
+   * @default ''
+   */
+  private _iconFolder = '';
 
   /**
    * Stores image format for the icon path.
@@ -93,10 +119,35 @@ export class IconComponent {
   private _imgFormat = 'svg';
 
   /**
-   * Builds the icon path.
-   * Concatenates base prefix with icon modifier, color modifier and image format.
+   * Indicates if an error occurred when loading the icon
+   * using the primary base URL/path for retrieving icons.
+   *
+   * @default false
+   */
+  private imageError = false;
+
+  public constructor(
+    @Inject(ICON_BASE_SRC) private iconBaseSRC: string,
+  ) { }
+
+  /**
+   * Builds the icon source URL/path.
+   * Concatenates base URL/path with icon folder, modifier, color modifier and image format.
    */
   public updatePath() {
-    this.path = this._pathPrefix + `${this._icon + (!!this._color ? '_' + this._color : '')}.` + this._imgFormat;
+    let prefix = this.imageError ? this._secondaryBase : this.iconBaseSRC;
+    if (this._iconFolder) {
+      prefix = prefix + this._iconFolder;
+    }
+    this.path = prefix + `${this._icon + (!!this._color ? '_' + this._color : '')}.` + this._imgFormat;
+  }
+
+  /**
+   * Runs when image fails to load using the primary base URL/path for retrieving icons.
+   * Sets image error flag and modifies image source path to secondary option.
+   */
+  public onImageError() {
+    this.imageError = true;
+    this.updatePath();
   }
 }
