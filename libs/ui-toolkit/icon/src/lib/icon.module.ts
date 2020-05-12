6cd29a8080f154bfac3e44d77014c332919b82bb
Guilherme Gois <guijocargo@gmail.com>
2020-05-12 18:16:33 -0300

    fix(icon): set default ICON_BASE_SRC token value

--- libs/ui-toolkit/icon/src/lib/icon.module.ts
+++ libs/ui-toolkit/icon/src/lib/icon.module.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { IconComponent } from './icon.component';
+import { ICON_BASE_SRC } from './token';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-
+  providers: [{ provide: ICON_BASE_SRC, useValue: '/' }],
   declarations: [IconComponent],
   exports: [IconComponent]
 })
