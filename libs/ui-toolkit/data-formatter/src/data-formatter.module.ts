461ea471e8053d194a1d1a978b53a5886fecf8d6
Gustavo C. Oliveira <gustavo.oliveira@gorilainvest.com.br>
2020-09-03 11:14:42 -0300

    improvement(data-formatter): add translate pipe to text type

--- libs/ui-toolkit/data-formatter/src/data-formatter.module.ts
+++ libs/ui-toolkit/data-formatter/src/data-formatter.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { MomentModule } from 'ngx-moment';
 
 import { GorCustomPipe } from './custom.pipe';
@@ -7,7 +8,7 @@ import { DataFormatterComponent } from './data-formatter.component';
 
 @NgModule({
   declarations: [DataFormatterComponent, GorCustomPipe],
-  imports: [CommonModule, MomentModule],
+  imports: [CommonModule, MomentModule, TranslateModule.forChild()],
   providers: [GorCustomPipe],
   exports: [DataFormatterComponent, GorCustomPipe]
 })
