b7298558496028e4f9dadd1e6ba8afc5abe7642b
Guilherme Gois <guijocargo@gmail.com>
2020-11-04 01:05:44 -0200

    chore(build): re-adapt build script to deal with new version of nx

--- scripts/build-all.js
+++ scripts/build-all.js
@@ -3,27 +3,20 @@ const rimraf = require('rimraf');
 const Rx = require('rxjs');
 const RxOp = require('rxjs/operators');
 const ngjson = require("../angular.json");
-const nxdepsjson = require("../dist/nxdeps.json");
+const { graph: nxdepsjson } = require('../dist/nxdeps.json');
 const nxjson = require("../nx.json");
-
 const clog = msg => console.log('\x1b[36m%s\x1b[0m', msg);
-
 const COMPILED_LIBS = Object.keys(nxjson.projects)
   .filter(lib => ngjson.projects[lib].projectType === 'library' && !!ngjson.projects[lib].architect.build)
   .reduce((acc, lib) => ({ ...acc, [lib]: false }), {});
-
-const getDeps = proj =>
-  nxdepsjson.dependencies[proj]
-    .map(dep => dep.projectName)
-    .filter(dep => COMPILED_LIBS[dep] === false);
-
+const getDeps = (proj) =>
+  nxdepsjson.dependencies[proj].map(dep => dep.target).filter(dep => COMPILED_LIBS[dep] === false);
 const DEP_GRAPH = Object.keys(COMPILED_LIBS).reduce((acc, k) => ({
   ...acc,
   [k]: getDeps(k)
 }));
 
 const libs = Object.keys(COMPILED_LIBS);
-
 rimraf('dist', { disableGlob: true }, async err => {
   if (err) {
     return;
@@ -32,7 +25,6 @@ rimraf('dist', { disableGlob: true }, async err => {
   let buildObservable = null;
   const asyncBuild = lib => {
     const buildOptions = ngjson.projects[lib].architect.build.options;
-
     return ngPackage
       .ngPackagr()
       .forProject(buildOptions.project)
@@ -44,7 +36,7 @@ rimraf('dist', { disableGlob: true }, async err => {
           process.exit(1);
         })
       );
-  };
+  }
 
   await Rx.timer(0, 0).pipe(
     RxOp.filter(() => buildObservable === null),
@@ -52,7 +44,6 @@ rimraf('dist', { disableGlob: true }, async err => {
   ).subscribe(() => {
     const lib = libs.shift();
     clog(`Checking if ${lib} can be build...`);
-
     if (COMPILED_LIBS[lib]) {
       clog(`${lib} was compiled previously, discarding it`);
       return;
